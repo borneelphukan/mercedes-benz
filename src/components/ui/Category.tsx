75a01bb6e3e0465221b72f858a218a82ba5d5295
Borneel B. Phukan <borneelphukan@gmail.com>
2025-01-27 19:50:23 +0100

    fixed

--- src/components/ui/Category.tsx
+++ src/components/ui/Category.tsx
@@ -35,20 +35,6 @@ function Category({ data, onCategoryChange }: Props) {
           </option>
         ))}
       </select>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        strokeWidth="1.5"
-        stroke="currentColor"
-        className="dropdown-icon"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="m19.5 8.25-7.5 7.5-7.5-7.5"
-        />
-      </svg>
     </div>
   );
 }
